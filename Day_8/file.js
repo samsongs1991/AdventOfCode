8fb9a374c350dbcd9911242ba4411a42a6857119
Samuel Song <samsongs@umich.edu>
2021-12-11 07:58:29 -0700

    Work on Day 8 pt 1

---
+++ Day_8/file.js
@@ -0,0 +1,16 @@
+const testSignals = [acedgfb,cdfbe,gcdfa,fbcad,dab,cefabd,cdfgeb,eafb,cagedb,ab];
+const testOutputs = [cdfeb,fcadb,cdfeb,cdbaf]
+
+const code = {
+    0: [a, b, c,    e, f, g], // 6 
+    1: [      c,       f   ], // 2 *
+    2: [a,    c, d, e,    g], // 5 
+    3: [a,    c, d,    f, g], // 5 
+    4: [b,    c, d,    f   ], // 4 *
+    5: [a, b,    d,    f, g], // 5 
+    6: [a, b,    d, e, f, g], // 6 
+    7: [a,    c,       f   ], // 3 *
+    8: [a, b, c, d, e, f, g], // 7 *
+    9: [a, b, c, d,    f, g]  // 6
+}
+
