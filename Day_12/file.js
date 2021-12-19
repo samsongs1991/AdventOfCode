234255d721f8151df3d0be5e23d7300ce4cafc32
Samuel Song <samsongs@umich.edu>
2021-12-19 13:38:10 -0500

    Setup day 12

---
+++ Day_12/file.js
@@ -0,0 +1,15 @@
+const Module = require('../module.js');
+// const data = Module.extract('./data.txt');
+// const testData = Module.extract('./testData.txt');
+
+// ===============
+// Day_12 - Part 1
+// ===============
+
+
+
+// ===============
+// Day_12 - Part 2
+// ===============
+
+
