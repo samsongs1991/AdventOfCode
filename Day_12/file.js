b76bc7194e6a56dd0291fc54fd4b56d1de581dc9
Samuel Song <samsongs@umich.edu>
2021-12-19 13:48:42 -0500

    Setup additional data files

--- Day_12/file.js
+++ Day_12/file.js
@@ -1,6 +1,8 @@
 const Module = require('../module.js');
 // const data = Module.extract('./data.txt');
-// const testData = Module.extract('./testData.txt');
+// const testData1 = Module.extract('./testData1.txt');
+// const testData2 = Module.extract('./testData2.txt');
+// const testData3 = Module.extract('./testData3.txt');
 
 // ===============
 // Day_12 - Part 1
